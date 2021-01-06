71d0a5116720c7edc5702d864150ba3cf610d619
NemanjaPavlovic98 <48028697+NemanjaPavlovic98@users.noreply.github.com>
2021-01-06 18:31:11 +0100

    Add files via upload

---
+++ Domen/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domen
+{
+    public enum Operation { Login, SendMessage, EndCommunication}
+    [Serializable]
+    public class Message
+    {
+        public string Text { get; set; }
+        public string Name { get; set; }
+        public Operation Operation { get; set; }
+        public bool IsSuccesful { get; set; }
+
+
+    }
+}
